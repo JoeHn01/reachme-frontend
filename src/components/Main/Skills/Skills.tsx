32839eac8e9ff48bd1c2eb649624a13d5691a4f0
JoeHn01 <yousef.hanafi2001@gmail.com>
2024-10-24 14:45:26 -0600

    Small edit on Actions. Added Skills section content

--- src/components/Main/Skills/Skills.tsx
+++ src/components/Main/Skills/Skills.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import styles from './Skills.module.css';
 
 const Skills: React.FC = () => {
+  const skillsList = ["Python", "Java", "React", "Node.js", "SQL", "JavaScript", "C/C++", "Next.js", "MongoDB"]
+
   return (
-    <div>
-      Skills Section
+    <div className={styles.skills}>
+      <div className={styles.labelContainer}>
+        <label className={styles.skillsLabel}>Trending Job Skills</label>
+        <p className={styles.skillsSubtitle}>For those deeply committed to sustainable practices</p>
+      </div>
+      <div className={styles.skillsList}>
+        {skillsList.map((skill, index) => {
+          return (
+            <div className={styles.skillWrapper} key={index}>{skill}</div>
+          );
+        })}
+      </div>
     </div>
   );
 };
